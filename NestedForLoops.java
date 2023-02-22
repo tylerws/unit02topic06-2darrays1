5da350b81321be1cfa6dd558b0e9d0a82b4a556d
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-02-22 11:47:15 -0600

    nested

---
+++ NestedForLoops.java
@@ -0,0 +1,9 @@
+public class NestedForLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+    }
+}
